b3a9c167653e508f8ee9fefa58da0aa027402b44
tclchiam <tclchiam@gmail.com>
2015-12-14 14:15:02 -0500

    TCHU - Rearranged Packages, added Google API to VCS

---
+++ app/src/main/java/edu/msu/hlavaty1/fire/gcm/GCMIntentService.java
@@ -0,0 +1,64 @@
+package edu.msu.hlavaty1.fire.gcm;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import edu.msu.hlavaty1.fire.ui.MapsActivity;
+
+public class GCMIntentService extends IntentService {
+
+    // Cases that match notification title
+    public static final String REFRESH_CASE = "refresh_fires";
+    public static final String ACTION_KEY = "action";
+
+    public GCMIntentService() {
+        super("GCMIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+
+        String messageType = gcm.getMessageType(intent);
+
+        if (extras != null && !extras.isEmpty()) {
+            // Since we're not using two way messaging, this is all we really to check for
+            if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+
+                String title = extras.getString("title");
+                if (title == null) {
+                    return;
+                }
+
+                switch (title) {
+                    case REFRESH_CASE:
+                        showToast(extras.getString("message"));
+                        Intent refreshIntent = new Intent(MapsActivity.RECEIVE);
+                        refreshIntent.putExtra(ACTION_KEY, REFRESH_CASE);
+                        sendBroadcast(refreshIntent);
+                        break;
+                    default:
+                        showToast("Unrecognized Notification");
+                        break;
+                }
+            }
+        }
+        GCMBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private void showToast(final String message) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
